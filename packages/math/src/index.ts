2ba3ca932db42acf6bf380d2bf99e785e9ad5700
King <47607185+xKingDark@users.noreply.github.com>
2026-04-29 21:47:40 +0100

    @foxystar/math - v0.1.1

--- packages/math/src/index.ts
+++ packages/math/src/index.ts
@@ -1,6 +1,9 @@
-export * from "./Math.ts"
-export * from "./phys/AABB.ts"
-export * from "./phys/Vector3.ts"
+export * from "./Math.ts";
+export * from "./noise/PerlinNoise.ts";
 
-export * from "./random/SimpleRandom.ts"
-export * from "./random/XoroshiroRandom.ts"
+export * from "./phys/AABB.ts";
+export * from "./phys/Vector3.ts";
+
+export * from "./random/IRandom.ts";
+export * from "./random/SimpleRandom.ts";
+export * from "./random/XoroshiroRandom.ts";
